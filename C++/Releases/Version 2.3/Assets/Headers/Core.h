eeb9dffed909b8f0cd48a67a946354a52304474e
RelativiBit <tjlarrechea@gmail.com>
2023-06-18 21:48:42 -0700

    Core File Update

--- C++/Releases/Version 2.3/Assets/Headers/Core.h
+++ C++/Releases/Version 2.3/Assets/Headers/Core.h
@@ -3,6 +3,19 @@
 #define CORE_H
 #include "../HPP/Hand.hpp"
 
+Hand update_stats(Hand& input); // 9
+Hand csv_stats(Hand& input); // 10
+std::tuple<Hand, Hand, Shoe> deal_hand(Hand& playerHand, Hand& dealerHand, Shoe& shoe); // 1
+void blackjack_strategy(Hand& playerHand, Hand& dealerHand); // 11
+std::tuple<Hand, Hand, Shoe, bool> dealer_showing_ace(Hand& playerHand, Hand& dealerHand, Shoe& shoe); // 2
+std::vector<Hand> split_hand(Hand& input); // 3
+std::tuple<std::vector<Hand>, Hand, Hand, Shoe, int> same_rank_check(Hand& playerHand, Hand& dealerHand, Shoe& shoe); // 4
+std::tuple<Hand, Hand, Shoe> player_hand_logic(Hand& playerHand, Hand& dealerHand, Hand& referenceHand, Shoe& shoe, int& hand_counter); // 5
+std::tuple<Hand, Shoe> dealer_hand_logic(std::vector<Hand>& playerHands, Hand& dealerHand, Shoe& shoe); // 6
+std::tuple<Hand, Hand, Shoe> hand_comparison_logic(Hand& playerHand, Hand& dealerHand, Shoe& shoe, int& playerHandCount, int& currentHandCounter); // 7
+std::tuple<Hand, Hand, Shoe> game_logic(Hand& playerHand, Hand& dealerHand, Shoe& shoe); // 8
+void play_game();
+
 void test_game();
 
 #endif
