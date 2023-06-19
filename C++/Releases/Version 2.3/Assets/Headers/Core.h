5e3e6e77961a5365d29180a9e32a340b22a46ea4
RelativiBit <tjlarrechea@gmail.com>
2023-06-19 00:49:21 +0000

    Core Functions Updates

--- C++/Releases/Version 2.3/Assets/Headers/Core.h
+++ C++/Releases/Version 2.3/Assets/Headers/Core.h
@@ -3,6 +3,9 @@
 #define CORE_H
 #include "../HPP/Hand.hpp"
 
+bool check_parameter_in_hand(const Hand& input, const std::string referenceParameter, const std::string checkingParameter);
+bool check_same_parameter_in_hand(Hand& input, const std::string referenceParameter, const std::string checkingParameter = "");
+
 void test_game();
 
 #endif
