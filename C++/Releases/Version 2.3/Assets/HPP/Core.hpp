8f8aad10ed33bfe5394c1016bf5f51aee1d27950
RelativiBit <tjlarrechea@gmail.com>
2023-06-18 21:37:26 -0700

    Core File Update

--- C++/Releases/Version 2.3/Assets/HPP/Core.hpp
+++ C++/Releases/Version 2.3/Assets/HPP/Core.hpp
@@ -2,8 +2,6 @@
 #include "../Headers/Core.h"
 
 void test_game() {
-    Hand testHand;
-    testHand.AddCardToHand(Card(Ranks[0], Suits[0]));
-    testHand.AddCardToHand(Card(Ranks[8], Suits[0]));
+    
 }
 
