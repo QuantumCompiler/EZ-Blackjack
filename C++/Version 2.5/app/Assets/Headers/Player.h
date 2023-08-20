e4085b8a19c3dff097145f1ac37b56c22f596915
RelativiBit <tjlarrechea@gmail.com>
2023-08-19 23:53:48 -0700

    Player Class Initial Commit

---
+++ C++/Version 2.5/app/Assets/Headers/Player.h
@@ -0,0 +1,17 @@
+// ----- ----- ----- ----- ----- ----- ----- Includes ----- ----- ----- ----- ----- ----- ----- ----- ----- //
+#ifndef PLAYER_H
+#define PLAYER_H
+#include "../HPP/Hand.hpp"
+
+struct PlayerParameters {
+
+};
+
+class Player {
+public:
+
+private:
+
+};
+
+#endif
