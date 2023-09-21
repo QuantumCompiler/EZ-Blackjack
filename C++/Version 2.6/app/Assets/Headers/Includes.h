12cec0ec19ca9300b66a6c5bed7d7b74e52bc670
RelativiBit <tjlarrechea@gmail.com>
2023-09-20 23:11:58 -0700

    Includes File Update 9/20/23

--- C++/Version 2.6/app/Assets/Headers/Includes.h
+++ C++/Version 2.6/app/Assets/Headers/Includes.h
@@ -5,7 +5,7 @@
 #define SHORT_TIME_SLEEP 500
 #define MEDIUM_TIME_SLEEP 1500
 #define LONG_TIME_SLEEP 3000
-#define PRINT_LINE_SLEEP 20
+#define PRINT_LINE_SLEEP 0
 #define SPRINT_LINE_SLEEP PRINT_LINE_SLEEP / 2
 
 #include <algorithm>
