7f94d3572c00e67510cad606fd4979242059c6c9
RelativiBit <tjlarrechea@gmail.com>
2023-09-23 22:44:16 -0700

    Version 2.6 Completion

--- C++/Version 2.6/app/Assets/Headers/Game.h
+++ C++/Version 2.6/app/Assets/Headers/Game.h
@@ -6,7 +6,6 @@
 void blackjack(); // Incorporates game modes, an introduction to the program
 void game_rules(); // Displays the rules for blackjack
 void game_options(); // Gives the user options for what type of game they would like to play
-void simulate_game(); // Provides a simulated game for a player
 void simulate_options(); // Gives the user options for what type of simulation they would like to perform
 void simulate_choices(int input); // Displays the strategy of a blackjack simulation 
 void single_player_versus_dealer(); // Provides a game for a player to play
